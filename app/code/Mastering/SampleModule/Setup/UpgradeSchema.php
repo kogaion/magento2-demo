7bf5dd2c1a4c059df1ce7df61f4c714944a51d68
lucian.toza <ltoza@optaros.com>
2018-07-05 15:39:45 +0300

    Added a new column to the sales_order_grid. Bumped module version.

--- app/code/Mastering/SampleModule/Setup/UpgradeSchema.php
+++ app/code/Mastering/SampleModule/Setup/UpgradeSchema.php
@@ -21,21 +21,31 @@ class UpgradeSchema implements UpgradeSchemaInterface
      */
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '1.0.0', '<>')) {
-            return;
-        }
-        
         $setup->startSetup();
-        
-        $setup->getConnection()->addColumn(
-            $setup->getTable('mastering_sample_item'),
-            'description',
-            [
-                'type' => Table::TYPE_TEXT,
-                'nullable' => true,
-                'comment' => 'Item Description'
-            ]
-        );
+
+        if (version_compare($context->getVersion(), '1.0.0', 'eq')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('mastering_sample_item'),
+                'description',
+                [
+                    'type' => Table::TYPE_TEXT,
+                    'nullable' => true,
+                    'comment' => 'Item Description'
+                ]
+            );
+        }
+
+        if (version_compare($context->getVersion(), '1.0.1', 'eq')) {
+            $setup->getConnection()->addColumn(
+                $setup->getTable('sales_order_grid'),
+                'base_tax_amount',
+                [
+                    'type' => Table::TYPE_DECIMAL,
+                    'comment' => 'Base Tax Amount'
+                ]
+            );
+        }
+
         
         $setup->endSetup();
     }
