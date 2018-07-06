ae83005194a2add595c9f7fb2dac47f8e8cda8e8
lucian.toza <ltoza@optaros.com>
2018-07-06 15:16:56 +0300

    Replace the frontend echo with a page response.

--- app/code/Mastering/HelloWorld/Controller/Hello/World.php
+++ app/code/Mastering/HelloWorld/Controller/Hello/World.php
@@ -10,12 +10,23 @@ namespace Mastering\HelloWorld\Controller\Hello;
 
 
 use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class World extends Action
 {
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+
+        parent::__construct($context);
+    }
+
     public function execute()
     {
-        echo "Yesss!";
-        var_dump(__METHOD__);
+        $page = $this->pageFactory->create();
+        return $page;
     }
 }
