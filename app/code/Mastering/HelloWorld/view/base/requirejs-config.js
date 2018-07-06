5a72eace71d84b12b065f56f4764fc95d2e83d3d
lucian.toza <ltoza@optaros.com>
2018-07-06 17:52:57 +0300

    Load my_module after the required dependency jquery has been loaded (to avoid async loading).

--- app/code/Mastering/HelloWorld/view/base/requirejs-config.js
+++ app/code/Mastering/HelloWorld/view/base/requirejs-config.js
@@ -2,5 +2,10 @@ console.log("Loaded requirejs-config");
 var config = {
     paths: {
         "my_module": "Mastering_HelloWorld/my_module"
+    },
+    shim:{
+        'my_module':{
+            'deps':['jquery']
+        }
     }
 };
--- app/code/Mastering/HelloWorld/view/frontend/web/test.js
+++ app/code/Mastering/HelloWorld/view/frontend/web/test.js
@@ -1,5 +1,11 @@
 console.log('loaded test.js');
 
-requirejs(['jquery'], function($) {
+requirejs(['jquery', 'my_module'], function($) {
     console.log('loaded jquery in a RequireJs module');
+});
+
+define('my_module', function(my_module) {
+   return {
+
+   } ;
 });
