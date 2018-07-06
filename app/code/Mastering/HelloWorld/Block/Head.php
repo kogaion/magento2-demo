06bc7af2739c5be513daf51c835d4fccdad02260
lucian.toza <ltoza@optaros.com>
2018-07-06 16:56:38 +0300

    Use the Asset\Repository to generate static content links in blocks.

--- app/code/Mastering/HelloWorld/Block/Head.php
+++ app/code/Mastering/HelloWorld/Block/Head.php
@@ -9,9 +9,22 @@
 namespace Mastering\HelloWorld\Block;
 
 
+use Magento\Framework\View\Asset\Repository;
 use Magento\Framework\View\Element\Template;
 
 class Head extends Template
 {
+    protected $assetRepository;
 
+    public function __construct(Template\Context $context, array $data = [], Repository $assetRepository)
+    {
+        $this->assetRepository = $assetRepository;
+        parent::__construct($context, $data);
+    }
+
+    public function getTestAssetUrl()
+    {
+        $asset = $this->assetRepository->createAsset("Mastering_HelloWorld::test.js");
+        return $asset->getUrl();
+    }
 }
