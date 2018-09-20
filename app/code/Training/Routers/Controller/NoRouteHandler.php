39ef8ce2699aa107049afe979f4cf6b958e3ea60
lucian.toza <ltoza@optaros.com>
2018-09-20 16:11:51 +0300

    Log the frontName of the routes.

--- app/code/Training/Routers/Controller/NoRouteHandler.php
+++ app/code/Training/Routers/Controller/NoRouteHandler.php
@@ -5,12 +5,26 @@ namespace Training\Routers\Controller;
 
 
 use Magento\Framework\App\Router\NoRouteHandler as AppNoRouteHandler;
+use Psr\Log\LoggerInterface;
 
 class NoRouteHandler extends AppNoRouteHandler
 {
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $config, LoggerInterface $logger)
+    {
+        parent::__construct($config);
+        $this->logger = $logger;
+    }
+
     /** @inheritdoc */
     public function process(\Magento\Framework\App\RequestInterface $request)
     {
+        $this->logger->info($request->getFrontName());
+
         if ($request->getFrontName() == "admin") {
             return false;
         }
