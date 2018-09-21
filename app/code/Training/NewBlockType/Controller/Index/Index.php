9b4bc2e9cca86824f6a3cfcd8ad72ac40f2c55f1
lucian.toza <ltoza@optaros.com>
2018-09-21 16:08:49 +0300

    Added a text block into the page.

--- app/code/Training/NewBlockType/Controller/Index/Index.php
+++ app/code/Training/NewBlockType/Controller/Index/Index.php
@@ -6,6 +6,7 @@ namespace Training\NewBlockType\Controller\Index;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Element\Text;
 use Magento\Framework\View\Result\Page;
 use Training\NewBlockType\Block\My\Type;
 
@@ -16,9 +17,15 @@ class Index extends Action
      */
     public function execute()
     {
-        /** @var Page $resultPage */
+        /** @var Page $resultPage
+         *  @var Text $textBlock
+         */
         $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
         $resultPage->getLayout()->addBlock(Type::class, "my_block_type", "content");
+
+        $textBlock = $resultPage->getLayout()->addBlock(Text::class, "my_text_block", "content");
+        $textBlock->setText("a text block");
+
         return $resultPage;
     }
 }
