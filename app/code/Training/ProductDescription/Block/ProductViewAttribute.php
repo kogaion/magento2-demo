4238a62382149bdfbd7f19fa505b96075185a94a
lucian.toza <ltoza@optaros.com>
2018-09-28 15:06:25 +0300

    Define the template used in Magento\Catalog\Block\Product\View\Attributes.

---
+++ app/code/Training/ProductDescription/Block/ProductViewAttribute.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Training\ProductDescription\Block;
+
+
+use Magento\Catalog\Block\Product\View\Attributes;
+
+class ProductViewAttribute extends Attributes
+{
+    public function setTemplate($template)
+    {
+        $template = "Training_ProductDescription::catalog/product/attributes.phtml";
+        $this->_logger->warning("$template");
+        return parent::setTemplate($template);
+    }
+}
